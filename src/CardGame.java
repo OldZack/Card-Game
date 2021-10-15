ba992cc95ca1d76577a5b112db527715884f4053
OldZack <31441355+OldZack@users.noreply.github.com>
2021-10-14 22:54:45 -0400

    Arraylist.get corrected

--- src/CardGame.java
+++ src/CardGame.java
@@ -1,2 +1,15 @@
-public class CardGame {
+import java.util.ArrayList;
+
+public abstract class CardGame {
+
+    protected ArrayList<Player> players = new ArrayList<Player>();
+    protected Dealer dealer = new Dealer();
+    protected Deck deck = new Deck();
+
+    public abstract void startGame();
+
+    public abstract void round();
+
+    public abstract void checkWinner();
+
 }
